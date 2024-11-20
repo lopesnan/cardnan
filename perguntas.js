3323e27784a339a105298ead05b46f2787506a86
lopesnan <renanlopes306@gmail.com>
2024-11-20 15:11:22 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,21 @@
+criaCartao(
+  'Astronomia', 
+  'O que é uma estrela?', 
+  'Uma estrela é uma esfera de gás quente que emite luz e calor devido à fusão nuclear em seu núcleo.'
+)
+criaCartao(
+  'História', 
+  'Quem foi o primeiro presidente dos Estados Unidos?', 
+  'O primeiro presidente dos Estados Unidos foi George Washington.'
+)
+criaCartao(
+  'Matemática', 
+  'O que é um número primo?', 
+  'Um número primo é um número natural maior que 1 que só é divisível por 1 e por ele mesmo.'
+)
+criaCartao(
+  'Física',
+  'O que é a lei da gravidade?', 
+  'A lei da gravidade afirma que todos os objetos com massa atraem uns aos outros com uma força proporcional à massa e inversamente proporcional ao quadrado da distância entre eles.'
+)
 
